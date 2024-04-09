92256377a6dd663d1315e379f37075b9025c12ff
Izosimov_Andrew <chernigovsky108@gmail.com>
2024-04-09 20:26:10 +0300

    first steps

---
+++ test/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import { MyButton } from 'ui-kit';
+
+function App() {
+  return (
+    <div className="App">
+      <MyButton color={'green'} big>Button</MyButton>
+    </div>
+  );
+}
+
+export default App;
